bd25569f44a77f19d67fc59d35850d0ac4b751eb
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2025-09-13 17:02:41 +0800

    React Successfully created

---
+++ react-quickstart/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+//making nested components
+function App() {
+  
+}
+export default App
