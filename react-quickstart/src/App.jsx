0b91ec080c8ae351ade0f5c7bd4cfc39a1e277b8
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2025-09-13 17:07:53 +0800

    Done creating and nesting components

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -4,7 +4,30 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 //making nested components
+function Header() {
+  return(
+    <h1>this is the header</h1>
+  )
+}
+function Main() {
+  return(
+    <h1>This is the Main content</h1>
+  )
+}
+
+function Footer() {
+  return(
+    <h1>This is the Footer</h1>
+  )
+}
 function App() {
-  
+  return(
+    <>
+    {/* Done creating and nesting components */}
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  )
 }
 export default App
