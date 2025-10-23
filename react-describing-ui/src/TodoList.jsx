df2400c5b0075cd689d5b55abe6f253cc4ea6750
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2025-10-23 19:43:12 +0800

    Done with how to use curly braces with JSX to access JavaScript functionality from your components

--- react-describing-ui/src/TodoList.jsx
+++ react-describing-ui/src/TodoList.jsx
@@ -14,21 +14,48 @@
 //     </ul>
 //   );  bad practice, no fragment and markup is not in jsx format
 
+// export default function TodoList() {
+//   return ( 
+//     <>
+//       <h1>Hedy Lamarr's Todos</h1>  
+//       <img
+//         src="https://i.imgur.com/yXOvdOSs.jpg"
+//         alt="Hedy Lamarr"
+//         className="photo"
+//       />
+//       <ul>
+//         <li>Invent new traffic lights</li>
+//         <li>Rehearse a movie scene</li>
+//         <li>Improve spectrum technology</li>
+//       </ul>
+//     </>
+//   ); //good usage of fragment and jsx format
+// }
+
+
+
+const person = {
+  name: 'Gregorio Y. Zara',
+  theme: {
+    backgroundColor: 'black',
+    color: 'pink'
+  }
+};
+
 export default function TodoList() {
-  return ( 
-    <>
-      <h1>Hedy Lamarr's Todos</h1>  
+  return (
+    <div style={person.theme}>
+      <h1>{person.name}'s Todos</h1>
       <img
-        src="https://i.imgur.com/yXOvdOSs.jpg"
-        alt="Hedy Lamarr"
-        className="photo"
+        className="avatar"
+        src="https://i.imgur.com/7vQD0fPs.jpg"
+        alt="Gregorio Y. Zara"
       />
       <ul>
-        <li>Invent new traffic lights</li>
-        <li>Rehearse a movie scene</li>
-        <li>Improve spectrum technology</li>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fuelled engine</li>
       </ul>
-    </>
-  ); //good usage of fragment and jsx format
+    </div>
+  );
 }
-
