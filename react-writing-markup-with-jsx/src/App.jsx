23d89a82222498183349e74f53cb0713041ce66b
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2025-11-09 23:53:22 +0800

    Done with converting HTML to JSX

---
+++ react-writing-markup-with-jsx/src/App.jsx
@@ -0,0 +1,16 @@
+
+{/* <div>
+  <h1>Welcome</h1>
+  <p>This is plain HTML</p>
+</div> */} 
+//Convert to JSX
+
+
+export default function App() {
+  return(
+    <div>
+      <h1>Welcome</h1>
+      <p>This is now valid JSX markup.</p>
+    </div>
+  )
+}
