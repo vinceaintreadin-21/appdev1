906d7dba861284dc7877d55b8607af6fc3c731f7
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2025-11-22 10:14:18 +0800

    Done with passing strings with quotes

---
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Avatar from './Avatar'
+function App() {
+  return (
+    <>
+      <Avatar />
+    </>
+  )
+}
+
+export default App
