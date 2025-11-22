0b2c976a9ee4ae7dca747992088c822116f92e92
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2025-11-22 10:24:00 +0800

    Done with using double curlies: CSS and other objects in JSX

--- react-javascript-in-jsx-with-curly-braces/src/App.jsx
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -3,10 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Avatar from './Avatar'
+import TodoList from './TodoList'
+import TodoList2 from './TodoList2'
 function App() {
   return (
     <>
       <Avatar />
+      <TodoList/>
+      <TodoList2 />
     </>
   )
 }
---
+++ react-javascript-in-jsx-with-curly-braces/src/TodoList2.jsx
@@ -0,0 +1,12 @@
+export default function TodoList2() {
+  return (
+    <ul style={{
+      backgroundColor: 'black',
+      color: 'pink'
+    }}>
+      <li>Improve the videophone</li>
+      <li>Prepare aeronautics lectures</li>
+      <li>Work on the alcohol-fuelled engine</li>
+    </ul>
+  );
+}
