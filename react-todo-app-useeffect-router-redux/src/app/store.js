28407100b97fafade38066d8ccd2679f75e0582b
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2025-11-28 13:07:01 +0800

    update: updating for the redux store

--- react-todo-app-useeffect-router-redux/src/app/store.js
+++ react-todo-app-useeffect-router-redux/src/app/store.js
@@ -1,5 +1,5 @@
-import { configureStore } from "@reduxjs/toolkit";
-import '../features/todos/todosSlice'
+import { configureStore } from "@reduxjs/toolkit"
+import todosReducer from '../features/todos/todosSlice'
 
 export const store = configureStore({
     reducer: {
