654e78e72dfd3d578dee6d0dacb858bc5d024aea
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2025-11-28 13:10:27 +0800

    hotfix: made changes in the App jsx

--- react-todo-app-useeffect-router-redux/src/App.jsx
+++ react-todo-app-useeffect-router-redux/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home'
-import Login from './pages/Home'
+import Login from './pages/Login'
 import Todos from './pages/Todos'
 
 function App() {
